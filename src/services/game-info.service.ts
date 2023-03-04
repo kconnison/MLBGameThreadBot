40d199d57b3309325b24db0f874dfd2376919999
kconnison <>
2023-03-03 22:31:23 -0500

    convert properties to getter methods, added GameContent object

--- src/services/game-info.service.ts
+++ src/services/game-info.service.ts
@@ -1,10 +1,11 @@
 import MLBStatsAPI from "mlb-stats-typescript-api/output";
-import { GameRestObject } from "mlb-stats-typescript-api/output/src";
+import { GameContentRestObject, GameRestObject } from "mlb-stats-typescript-api/output/src";
 import { LoggerService } from "./logger.service";
 
 export class GameInfoService {
     private logger: LoggerService;
     private gameInfo: GameRestObject | undefined;
+    private gameContent: GameContentRestObject | undefined;
 
     constructor() {
         this.logger = new LoggerService(GameInfoService.name);
@@ -14,38 +15,85 @@ export class GameInfoService {
         return this.gameInfo?.gamePk || 0;
     }
 
-    get datetime() {
+    getDateTime() {
         return this.gameInfo?.gameData?.datetime || {};
     }
 
-    get gameStatus() {
+    getGameStatus() {
         return this.gameInfo?.gameData?.status || {};
     }
 
-    get homeTeam() {
+    /**
+     * Determine if game status is "Preview"
+     * @returns 
+     */
+    isGameStatusPreview() {
+        return (this.getGameStatus().abstractGameState == "Preview");
+    }
+
+    /**
+     * Determine if game status is "Live"
+     * @returns 
+     */
+    isGameStatusLive() {
+        return (this.getGameStatus().abstractGameState == "Live");
+    }
+
+    /**
+     * Determine if game status is "Final"
+     * @returns 
+     */
+    isGameStatusFinal() {
+        return (this.getGameStatus().abstractGameState == "Final");
+    }
+
+    getHomeTeam() {
         return this.gameInfo?.gameData?.teams?.home || {};
     }
 
-    get awayTeam() {
+    getAwayTeam() {
         return this.gameInfo?.gameData?.teams?.away || {};
     }
 
-    get plays() {
+    getPlayers() {
+        return this.gameInfo?.gameData?.players || {};
+    }
+
+    getVenue() {
+        return this.gameInfo?.gameData?.venue || {};
+    }
+
+    getWeather() {
+        return (this.gameInfo?.gameData?.weather as any) || {};
+    }
+
+    getGameInfo() {
+        return (this.gameInfo?.gameData?.gameInfo as any) || {};
+    }
+
+    getPlays() {
         return this.gameInfo?.liveData?.plays || {};
     }
 
-    get linescore() {
+    getLinescore() {
         return this.gameInfo?.liveData?.linescore || {};
     }
 
-    get boxscore() {
+    getBoxscore() {
         return this.gameInfo?.liveData?.boxscore || {};
+    }
+
+    getMedia() {
+        return (this.gameContent as any)?.media?.epg || []
     }
 
     public async load(gamePk: number) {
         this.logger.debug(`Loading game info for gamePk: ${gamePk} ...`);
-        this.gameInfo = (await MLBStatsAPI.GameService.liveGameV1(gamePk));
-        return this.gameInfo;        
+
+        let pGameInfo = MLBStatsAPI.GameService.liveGameV1(gamePk);
+        let pGameContent = MLBStatsAPI.GameService.content(gamePk);
+        [this.gameInfo, this.gameContent] = await Promise.all([pGameInfo, pGameContent]);
+        return;        
     }
 
     public update() {
