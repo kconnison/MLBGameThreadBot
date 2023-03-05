6994f52dfc3a4ebe9fb8050690e6ccd2f2fe9ea6
kconnison <>
2023-03-05 14:44:00 -0500

    parse player info & broadcast info into separate structures

---
+++ src/models/player-info.model.ts
@@ -0,0 +1,22 @@
+import { BoxscorePlayer, GamePlayerRestObject } from "mlb-stats-typescript-api/output/src";
+
+export class PlayerInfo {
+    private boxscore: BoxscorePlayer = {};
+
+    constructor(private profile: GamePlayerRestObject) {
+        return this;
+    }
+
+    public getProfile() {
+        return this.profile;
+    }
+
+    public getBoxscore() {
+        return this.boxscore;
+    }
+
+    public setBoxscore(newBoxscore: BoxscorePlayer) {
+        this.boxscore = newBoxscore;
+        return this;
+    }
+}
--- src/services/game-info.service.ts
+++ src/services/game-info.service.ts
@@ -1,26 +1,53 @@
 import MLBStatsAPI from "mlb-stats-typescript-api/output";
-import { GameContentRestObject, GameRestObject } from "mlb-stats-typescript-api/output/src";
+import { BoxscorePlayer, BroadcastRestObject, GameContentRestObject, GamePlayerRestObject, GameRestObject, ScheduleRestGameObject } from "mlb-stats-typescript-api/output/src";
+import { PlayerInfo } from "../models/player-info.model";
 import { LoggerService } from "./logger.service";
 
 export class GameInfoService {
     private logger: LoggerService;
-    private gameInfo: GameRestObject | undefined;
-    private gameContent: GameContentRestObject | undefined;
+
+    private scheduleObject: ScheduleRestGameObject | undefined;
+    private gameObject: GameRestObject | undefined;
+
+    private broadcastInfo: GameBroadcastInfo | undefined;
+    private playerInfo: Map<number, PlayerInfo> = new Map();
 
     constructor() {
         this.logger = new LoggerService(GameInfoService.name);
     }
 
     get gamePk() {
-        return this.gameInfo?.gamePk || 0;
+        return this.gameObject?.gamePk || 0;
+    }
+
+    public async load(gamePk: number) {
+        this.logger.debug(`Loading game info for gamePk: ${gamePk} ...`);
+
+        let pSchedule = MLBStatsAPI.ScheduleService.schedule(1, [gamePk], { hydrate: "broadcasts,game(content(highlights(highlights)))" });
+        let pGameInfo = MLBStatsAPI.GameService.liveGameV1(gamePk);
+
+        let schedule, gameInfo;
+        [schedule, gameInfo] = await Promise.all([pSchedule, pGameInfo]);
+
+        this.scheduleObject = schedule?.dates?.at(0)?.games?.find(gm => { return gm.gamePk == this.gamePk; }) || {};
+        this.gameObject = gameInfo;
+
+        this.parseBroadcasts((this.scheduleObject as any)?.broadcasts);
+        this.parsePlayerInfo(gameInfo);
+
+        return;
+    }
+
+    public update() {
+        this.load(this.gamePk);
     }
 
     getDateTime() {
-        return this.gameInfo?.gameData?.datetime || {};
+        return this.gameObject?.gameData?.datetime || {};
     }
 
     getGameStatus() {
-        return this.gameInfo?.gameData?.status || {};
+        return this.gameObject?.gameData?.status || {};
     }
 
     /**
@@ -48,43 +75,43 @@ export class GameInfoService {
     }
 
     getHomeTeam() {
-        return this.gameInfo?.gameData?.teams?.home || {};
+        return this.gameObject?.gameData?.teams?.home || {};
     }
 
     getAwayTeam() {
-        return this.gameInfo?.gameData?.teams?.away || {};
+        return this.gameObject?.gameData?.teams?.away || {};
     }
 
     getPlayers() {
-        return this.gameInfo?.gameData?.players || {};
+        return this.gameObject?.gameData?.players || {};
     }
 
     getVenue() {
-        return this.gameInfo?.gameData?.venue || {};
+        return this.gameObject?.gameData?.venue || {};
     }
 
     getWeather() {
-        return (this.gameInfo?.gameData?.weather as any) || {};
+        return (this.gameObject?.gameData?.weather as any) || {};
     }
 
     getGameInfo() {
-        return (this.gameInfo?.gameData?.gameInfo as any) || {};
+        return (this.gameObject?.gameData?.gameInfo as any) || {};
     }
 
     getProbablePitchers() {
-        return (this.gameInfo?.gameData?.probablePitchers as any) || {};
+        return (this.gameObject?.gameData?.probablePitchers as any) || {};
     }
 
     getPlays() {
-        return this.gameInfo?.liveData?.plays || {};
+        return this.gameObject?.liveData?.plays || {};
     }
 
     getLinescore() {
-        return this.gameInfo?.liveData?.linescore || {};
+        return this.gameObject?.liveData?.linescore || {};
     }
 
     getBoxscore() {
-        return this.gameInfo?.liveData?.boxscore || {};
+        return this.gameObject?.liveData?.boxscore || {};
     }
 
     getHomePlayerInfo(id: number) {
@@ -96,25 +123,82 @@ export class GameInfoService {
     }
 
     private getPlayerInfo(teamDesignation: "home" | "away", id: number) {
-        let details = (this.gameInfo?.gameData?.players as any)[`ID${id}`];
-        let boxscore = ((this.gameInfo?.liveData?.boxscore?.teams || {})[teamDesignation]?.players as any)[`ID${id}`];
+        let details = (this.gameObject?.gameData?.players as any)[`ID${id}`];
+        let boxscore = ((this.gameObject?.liveData?.boxscore?.teams || {})[teamDesignation]?.players as any)[`ID${id}`];
         return { details, boxscore };
     }
 
-    getMedia() {
-        return (this.gameContent as any)?.media?.epg || []
+    public getBroadcasts(): GameBroadcastInfo {
+        let defaultInfo = { 
+            tv: { home: [], away: [], national: [] }, 
+            radio: { home: [], away: [], national: [] }
+        };
+        return this.broadcastInfo || defaultInfo;
     }
 
-    public async load(gamePk: number) {
-        this.logger.debug(`Loading game info for gamePk: ${gamePk} ...`);
+    private parseBroadcasts(broadcasts: BroadcastRestObject[]) {
+        let broadcastInfo: any = { 
+            tv: { home: [], away: [], national: [] }, 
+            radio: { home: [], away: [], national: [] }
+        };
 
-        let pGameInfo = MLBStatsAPI.GameService.liveGameV1(gamePk);
-        let pGameContent = MLBStatsAPI.GameService.content(gamePk);
-        [this.gameInfo, this.gameContent] = await Promise.all([pGameInfo, pGameContent]);
-        return;        
+        broadcasts.forEach((broadcast: any) => {
+            let isNational = broadcast.isNational;
+            let market = (isNational? "national" : broadcast.homeAway);
+
+            if( ["AM", "FM"].includes(broadcast.type) ) {
+                broadcastInfo.radio[market].push(broadcast.name);
+            } else if( ["TV"].includes(broadcast.type) ) {
+                broadcastInfo.tv[market].push(broadcast.name);
+            }
+        });
+
+        this.broadcastInfo = broadcastInfo;
     }
 
-    public update() {
-        this.load(this.gamePk);
+    private parsePlayerInfo(gameInfo: GameRestObject) {
+        let homePlayerBoxscores = gameInfo.liveData?.boxscore?.teams?.home?.players || {};
+        let awayPlayerBoxscores = gameInfo.liveData?.boxscore?.teams?.away?.players || {};
+        let playerBoxscores: any = {
+            ...homePlayerBoxscores,
+            ...awayPlayerBoxscores
+        };
+
+        let players: any = gameInfo.gameData?.players || {};
+        for (let id in players) {
+            let playerProfile: GamePlayerRestObject = players[id];
+            let playerBoxscore: BoxscorePlayer = playerBoxscores[id];
+
+            let playerId = playerProfile.id || 0;
+            let playerInfo = new PlayerInfo(playerProfile).setBoxscore(playerBoxscore);
+            this.playerInfo.set(playerId, playerInfo);
+        }
     }
+
+    private updatePlayerBoxscores(gameInfo: GameRestObject) {
+        let homePlayerBoxscores = gameInfo.liveData?.boxscore?.teams?.home?.players || {};
+        let awayPlayerBoxscores = gameInfo.liveData?.boxscore?.teams?.away?.players || {};
+        let playerBoxscores: any = {
+            ...homePlayerBoxscores,
+            ...awayPlayerBoxscores
+        };
+
+        for (let id in playerBoxscores) {
+            let playerBoxscore: BoxscorePlayer = playerBoxscores[id];
+            let playerId = playerBoxscore.person?.id || 0;
+
+            this.playerInfo.get(playerId)?.setBoxscore(playerBoxscore);
+        }
+    }
+}
+
+interface GameBroadcastInfo {
+    tv: GameBroadcastFeeds;
+    radio: GameBroadcastFeeds;
+}
+
+interface GameBroadcastFeeds {
+    home: BroadcastRestObject[];
+    away: BroadcastRestObject[];
+    national: BroadcastRestObject[];
 }
